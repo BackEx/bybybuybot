75e6033803c49118a108495e90f98c8f00f15949
Grigory Ozhegov <grigory@ozhegov.name>
2016-09-17 22:46:05 +0300

    /hackend

--- bots/bankexcustomer.py
+++ bots/bankexcustomer.py
@@ -1,9 +1,12 @@
 # coding=utf-8
+from pytz import timezone
+
 from models.awa import User
 from models.content import Text
 from roboman.bot import BaseBot
 import logging
 from settings import options
+import datetime
 
 logger = logging.getLogger('bank_ex_customer')
 
@@ -46,10 +49,22 @@ class BankExCustomer(BaseBot):
 
         if cls.match_command('/start', text):
             cls._on_start(**data)
+        elif cls.match_command('/hackend', text):
+            cls._on_hachend(**data)
 
     @classmethod
     def _on_start(cls, **kwargs):
         cls.send(
             chat_id=kwargs.get('chat_id'),
             text=Text.format('CL_MSG_WELCOME'),
+        )
+
+    @classmethod
+    def _on_hachend(cls, **kwargs):
+        now = datetime.datetime.now()
+        end = datetime.datetime(2016, 9, 18, 12, 0)
+        diff = (end - now)
+        cls.send(
+            chat_id=kwargs.get('chat_id'),
+            text=u"До конца хакатона %s:%s" % (diff.seconds / 3600, diff.seconds % 3600 / 60),
         )
