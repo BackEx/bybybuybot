2c15296a9a77d9c91d5a3f9728de424608b9fca7
Grigory Ozhegov <grigory@ozhegov.name>
2016-09-18 11:40:19 +0300

    api for update salesman

--- handlers/salesman_handler.py
+++ handlers/salesman_handler.py
@@ -12,7 +12,8 @@ class SalesmanHandler(AuthMixin, BankExObjectHandler):
     @property
     def post_methods(self):
         methods = {
-            'register': self.register
+            'register': self.register,
+            'update': self.update_salesman
         }
         methods.update(super(SalesmanHandler, self).post_methods)
         return methods
@@ -50,3 +51,21 @@ class SalesmanHandler(AuthMixin, BankExObjectHandler):
         salesman = Salesman(telegram_id=telegram_id, telegram_nick=telegram_nick, about=about)
         salesman.save()
         self.send_success_response(data={'id': salesman.get_id()})
+
+    def update_salesman(self):
+        telegram_id = self.get_str_argument('telegram_id')
+
+        try:
+            salesman = Salesman.objects.get(telegram_id=telegram_id)
+        except Salesman.DoesNotExist:
+            raise BankExServerError(BankExServerError.NOT_FOUND)
+
+        telegram_nick = self.get_str_argument('telegram_nick', default=None)
+        about = self.get_str_argument('about', default=None)
+
+        if telegram_nick:
+            salesman.telegram_nick = telegram_nick
+        if about:
+            salesman.about = about
+
+        salesman.save()
